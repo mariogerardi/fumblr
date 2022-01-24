a87bf6eca016a2fb05ea95355629c2b77bcb9cd8
maxmcgrath1 <tuc06096@temple.edu>
2022-01-24 15:55:00 -0500

    updated blog controller and server.js

--- controllers/blog_controller.js
+++ controllers/blog_controller.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { Blog } = require('../models')
+
+router.get('/', (req, res) => {
+    
+    Blog.find({}, (error, foundBlogs) => {
+        if(error) return console.log(error);
+
+        console.log(foundBlogs)
+        context = {
+            blogs: foundBlogs
+        }
+        res.render('index.ejs', context);
+    })
+});
+
+router.get("/dashboard", (req, res) => {
+    res.render("dashboard.ejs")
+});
+
+module.exports = router;
