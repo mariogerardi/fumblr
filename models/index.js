34079be18c24ee29bb78dee2924f8ea44dc87278
Alos22 <aniriver689@gmail.com>
2022-01-24 15:18:25 -0500

    adding asset to index

--- models/index.js
+++ models/index.js
@@ -4,5 +4,6 @@ module.exports = {
     //these are the exports for all models
     Content: require('./content_model.js'),
     User: require('./user_model.js'),
-    Blog: require('./blog_model')
+    Blog: require('./blog_model'),
+    Assets: require('./asset_model')
 }
