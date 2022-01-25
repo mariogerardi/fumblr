10679c58f516dee929b57d1db1eebda856420974
maxmcgrath1 <tuc06096@temple.edu>
2022-01-25 16:26:40 -0500

    posting works now

--- controllers/content_controller.js
+++ controllers/content_controller.js
@@ -2,17 +2,34 @@ const express = require('express');
 const router = express.Router();
 const { Content } = require('../models')
 
-router.get("/content", (req, res) => {
-    res.send("I am a route now")
-});
+router.get('/', (req, res) => {
+    
+    Content.find({}, (error, foundContent) => {
+        if(error) return console.log(error);
 
-
-router.get("/create_post", (req, res) => {
-    res.render("create_post.ejs")
+        // console.log(foundContent)
+        const context = {content: foundContent}
+        res.render('blog.ejs', context);
+    })
 });
 
 router.get("/new-text-post", (req, res) => {
     res.render("new-content/new_text.ejs")
 });
+
+router.post('/', (req, res) => {
+    Content.create(req.body, (error, createdContent) => {
+        if(error) console.log(error);
+        console.log(createdContent);
+        
+        
+        res.redirect("/content");
+    })
+});
+
+router.get("/create_post", (req, res) => {
+    res.render("create_post.ejs")
+});
+
 
 module.exports = router;
